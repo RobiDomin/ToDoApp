5c633f38af8d0cd41cb531b5606fd38731b20b3c
RobiDomin <104741060+RobiDomin@users.noreply.github.com>
2025-01-11 19:28:29 +0100

    Delete ToDoItem.cs

--- ToDoItem.cs
+++
@@ -1,7 +0,0 @@
-﻿public class ToDoItem
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public bool IsCompleted { get; set; }
-}
