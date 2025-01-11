5d53bbe44611293323fd28a19dd08ec0e2c0aa6a
RobiDomin <104741060+RobiDomin@users.noreply.github.com>
2025-01-11 17:08:07 +0100

    Zrobiony backend

---
+++ ToDoItem.cs
@@ -0,0 +1,7 @@
+﻿public class ToDoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
